529e5dc2c0c1e90feeba178997ed56cd3cc58321
mitch2446 <mitchellreid2468@gmail.com>
2016-10-31 23:47:46 -0400

    Initial commit

---
+++ processesTree.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+
+function recur {
+
+    cpid=${pids[$1]} #current pid
+    cppid=${ppids[$1]} #current ppid
+
+    #prints out the processes into the tree
+    #echo "Name:" ${commands[$1]} "PID:" ${pids[$1]} "PPID:" ${ppids[$1]} "STARTED:" ${starts[$1]} "BY:" ${users[$1]}
+    echo "Name:" $(basename ${commands[$1]}) "PID:" ${pids[$1]} "PPID:" ${ppids[$1]} "STARTED:" ${starts[$1]} "BY:" ${users[$1]}
+
+    #for loop that will go through and check if the current process has any child processes
+    for (( loc=1; loc<118; loc++ )) ; do
+
+        #if it does have child processes
+        if (( $cpid == ${ppids[loc]} )) ; then
+            printf "|     |" #prints space to make tree
+            recur $loc #and recursive call with info for the pid of current command to see if that one has child commands
+        fi
+
+    done
+
+}
+
+
+pids=($(ps axo pid))     #2
+ppids=($(ps axo ppid))  #3
+users=($(ps axo user))   #5    }order to be printed in tree
+starts=($(ps axo start)) #4
+commands=($(ps axo comm)) #1
+
+#amount of processes running
+len=${#pids[*]}
+
+#calling recur
+var=1
+recur var
+
