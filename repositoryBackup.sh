0506351e8c611a58ef37518f8760d64b7caa8aba
b0chen <bochen@live.ca>
2016-10-31 21:50:27 -0400

    Update repositoryBackup.sh

--- repositoryBackup.sh
+++ repositoryBackup.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
+#cron job 0 1 * * * ./repositoryBackup.sh >> backupsLog
 
-#cron job
-
-repository=$1 
-
+repository=$1
 github=$2
-
 username=$3
-
 password=$4
 
-date=$(date +%y%m%d%h)
+date=$(date +%Y%M%D%H)
 
 tar -czf backup$date.tgz/  --exclude= backup*.tgz
+echo Backup backup$date.tgz created successfully!
 
 git add backup$date.tgz
 
 git commit backup$date.tgz
+echo Backup backup$date.tgz committed to the local git repository
 
-git push https://$username:$password@$github --all  
+git push https://$username:$password@$github --all
+echo Backup backup$date.tgz pushed to the remote git repository $repository
